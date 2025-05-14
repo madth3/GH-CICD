26e3a9fd3de100b37f4bb21ccc8bf03ab3971a50
Julio Cesar Aguilar Cabrera <julio.aguilar@lania.edu.mx>
2025-05-14 13:56:51 -0600

    Repo commented for clarity

--- src/main/java/mx/lania/taller/repositorios/RepositorioVehiculos.java
+++ src/main/java/mx/lania/taller/repositorios/RepositorioVehiculos.java
@@ -9,13 +9,14 @@ import java.util.List;
 
 public interface RepositorioVehiculos extends JpaRepository<Vehiculo,String> {
 
+    // Example of SQL native query
     @Query(value="SELECT * FROM vehiculos" +
             " WHERE kilometraje >= :k1 AND kilometraje <= :k2", nativeQuery = true)
     public List<Vehiculo> buscarPorKilometraje(
             @Param("k1") Double k1, @Param("k2") Double k2
     );
 
-
+    // Example of native query with partial data
     @Query(value="SELECT placas,cliente_id,color,kilometraje FROM vehiculos" +
             " WHERE kilometraje >= :k1 AND kilometraje <= :k2", nativeQuery = true)
     public List<Object[]> buscarPorKilometrajeDos(
